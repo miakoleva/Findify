f9edf15eeae5bfb637174237027b4e5728265e5c
Mia Koleva <mia.koleva@sorsix.com>
2024-04-22 14:43:50 +0200

    Initial commit

---
+++ backend/src/main/kotlin/com/sorsix/finalproject/backend/TestController.kt
@@ -0,0 +1,18 @@
+package com.sorsix.finalproject.backend
+
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class TestController {
+
+    @GetMapping("/test")
+    fun test(response: HttpServletResponse): ResponseEntity<String> {
+        return ResponseEntity("BRAVO DARIO!!!", HttpStatus.OK)
+    }
+}
