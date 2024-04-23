00d6dee8ba2b2396353393d35be099751fe3f248
Dario Dimitrovski <dariosorsix@Darios-Laptop.home>
2024-04-23 20:49:50 +0200

    Added Sign Up in nav-bar

--- frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LoginComponent } from '../login-modal/login-modal.component';
+import { RegisterComponent } from '../register-modal/register-modal.component';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink, LoginComponent],
+  imports: [RouterLink, LoginComponent, RegisterComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
