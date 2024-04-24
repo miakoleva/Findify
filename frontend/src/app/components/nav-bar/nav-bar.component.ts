0852a112a97ef38e031e1f95c6f167e2d03dcdb1
Dario Dimitrovski <dariosorsix@Darios-Laptop.home>
2024-04-24 20:50:30 +0200

    Added 'Make a post' button and form

--- frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LoginComponent } from '../login-modal/login-modal.component';
 import { RegisterComponent } from '../register-modal/register-modal.component';
+import { AddPostComponent } from '../add-post/add-post.component';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink, LoginComponent, RegisterComponent],
+  imports: [RouterLink, LoginComponent, RegisterComponent, AddPostComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
