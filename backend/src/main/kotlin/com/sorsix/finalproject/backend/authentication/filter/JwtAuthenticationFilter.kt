c37c1995ea6fa5c8ca29cf9a53c46068f8be51fd
Dario Dimitrovski <dario.dimitrovski@sorsix.com>
2024-05-14 13:25:05 +0200

    Post bug

--- backend/src/main/kotlin/com/sorsix/finalproject/backend/authentication/filter/JwtAuthenticationFilter.kt
+++ backend/src/main/kotlin/com/sorsix/finalproject/backend/authentication/filter/JwtAuthenticationFilter.kt
@@ -38,6 +38,7 @@ class JwtAuthenticationFilter(private val authenticationManager: AuthenticationM
                 request.servletPath.equals("/api/lost-items") ||
                 request.servletPath.equals("/api/found-items") ||
                 request.servletPath.equals("/api/categories") ||
+                request.servletPath.startsWith("/api/posts") ||
                 request.servletPath.startsWith("/api/") && request.servletPath.endsWith("/image")
     }
 
